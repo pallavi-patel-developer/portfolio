261fbe038e729766c5461184eaf681f5f36d76d3
Pallavi Patel <pallavipatel1713@gmail.com>
2026-02-15 20:11:43 +0530

    feat: add essential image assets including logos, favicon, and Vercel SVG, and create a new Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center py-6 px-12 max-w-7xl mx-auto">
-      <div className="text-2xl font-bold tracking-wide">PALLAVI PATEL</div>
+      <div className="text-2xl font-bold tracking-wide flex">
+        <img src="logo-symbol.png" className="w-17 h-11 "></img>
+        <span className="flex mt-1">PALLAVI PATEL</span>
+      </div>
       <div className="flex gap-8 text-sm font-medium">
         <Link href="#" className="hover:text-accent transition-colors">Home</Link>
         <Link href="#about" className="hover:text-accent transition-colors">About Me</Link>
